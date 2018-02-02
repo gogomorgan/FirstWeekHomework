e6ee6d0c5480353405029e887500003072d3213d
gogomorgan <macrimorganb@gmail.com>
2018-02-02 15:18:35 -0500

    Uploaded Unity files

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GUIText potatoscoreText;
+    public GUIText eggplantscoreText;
+    public int potatoscore;
+    public int eggplantscore;
+
+    // Use this for initialization
+    void Start () {
+        potatoscore = 0;
+        eggplantscore = 0;
+        UpdateScore();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScore()
+    {
+        potatoscoreText.text = "Potato Score: " + potatoscore;
+        eggplantscoreText.text = "Eggplant Score: " + eggplantscore;
+    }
+
+}
---
+++ Assets/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour {
+
+    public float moveSpeed = 0.1f;
+    public KeyCode rightKey = KeyCode.RightArrow;
+    public KeyCode leftKey = KeyCode.LeftArrow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(rightKey))
+        {
+            transform.Translate(moveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(leftKey))
+        {
+            transform.Translate(-moveSpeed, 0, 0);
+        }
+        
+        if (transform.position.y < -2.47 && gameObject.name == "eggplant")
+        {
+            SceneManager.LoadScene("game over");
+        }
+
+        if (transform.position.y < -2.47 && gameObject.name == "potato5")
+        {
+            SceneManager.LoadScene("eggwin");
+        }
+    }
+}
